39f14c0564b4e2913eb906e36e0cc9370406970f
Ben Schwartz <benschwart@gmail.com>
2014-01-27 00:43:37 -0500

    Redid processTrial.m

--- processTrial.m
+++ processTrial.m
@@ -1,184 +1,131 @@
-function processTrial(working_dir, trial_name)
+function processTrial(working_dir, trial_dir, trial_name)
 start_path=pwd();
 cObject = onCleanup(@()cd(start_path));
 if(~checkForDir(working_dir))
     return;
 end
 cd(working_dir);
-trial_dir=[trial_name '_processed'];
 if(~exist(trial_dir,'dir'))
     disp(['directory ' trial_dir ' does not exist in ' working_dir]);
     return;
 end
 cd(trial_dir);
-BRAIN_DIR='brain';
-OUTER_DIR='outer';
-INNER_DIR='inner';
-src_dirs={BRAIN_DIR,OUTER_DIR,INNER_DIR};
-IN_DIR='in';
-OUT_DIR='out';
-dst_dirs={IN_DIR,OUT_DIR};
-if(~checkForDirs(src_dirs))
-    return;
-end
-for i=1:size(dst_dirs,2)
-    if(~exist(dst_dirs{i},'dir'))
-        mkdir(dst_dirs{i});
-    end
-end
-% Finished Checking/Making Filesytem - ready to correlate
-BRAIN_FILES=getNodeFiles(BRAIN_DIR);
-INNER_FILES=getNodeFiles(INNER_DIR);
-OUTER_FILES=getNodeFiles(OUTER_DIR);
-total=length(INNER_FILES)+length(OUTER_FILES);
-counter=0;
-% Find last completed correlation
-i_low=1;
-i_high=length(INNER_FILES);
-i_mid=1;
-j_mid=1;
-j_low=1;
-j_high=length(BRAIN_FILES);    
-while(i_low<=i_high)
-    i_mid = idivide(int32(i_high+i_low),2,'floor');
-    if(alreadyDone(IN_DIR,INNER_FILES{i_mid},BRAIN_FILES{j_low})&& ...
-            ~alreadyDone(IN_DIR,INNER_FILES{i_mid},BRAIN_FILES{j_high}))
-        % Somewhere on this external node
-        counter = counter+i_mid;
-        disp([num2str(double(counter)/total*100) '%']);
-        break
-    else
-        if(alreadyDone(IN_DIR,INNER_FILES{i_mid},BRAIN_FILES{j_low}))
-           % After this external node
-           i_low=i_mid+1;
-        else
-           % Before this external node
-           i_high=i_mid-1;
-        end
-    end
-    
-end
-while(j_low<=j_high)
-    j_mid =  idivide(int32(j_high+j_low),2,'floor');
-    if(alreadyDone(IN_DIR,INNER_FILES{i_mid},BRAIN_FILES{j_mid}))
-        if(j_mid==length(BRAIN_FILES) || ~alreadyDone(IN_DIR,INNER_FILES{i_mid},BRAIN_FILES{j_mid+1}))
-            break;
-        else
-            j_low=j_mid+1;
-        end
-    else
-        j_high=j_mid-1;
-    end
-end
-if(~alreadyDone(IN_DIR,INNER_FILES{i_mid},BRAIN_FILES{j_mid}))
-    for i=i_mid:length(INNER_FILES)
-        for j=j_mid:length(BRAIN_FILES)
-            infl=fullfile(pwd(),INNER_DIR,INNER_FILES{i});
-            brainf=fullfile(pwd(),BRAIN_DIR,BRAIN_FILES{j});
-            [outputf,n1,n2]=getOutName(INNER_FILES{i},BRAIN_FILES{j});
-            outputfullf=fullfile(pwd(),IN_DIR,outputf);
-            correlate(infl,brainf, outputfullf, n1, n2);
-        end
-        counter = counter+1;
-        disp([num2str(double(counter)/total*100) '%']);
-    end
+% Info File
+infoFile = fullfile('info.mat');
+if(~exist(infoFile,'file'))
+    NAME=trial_name;
+    save(infoFile,'NAME');
 else
-    counter = counter + i_mid;
-    disp([num2str(double(counter)/total*100) '%']);
+    disp(['found ' infoFile]);
+    load(infoFile); 
 end
-
-%OUTER FILES
-
-i_low=1;
-i_high=length(OUTER_FILES);
-i_mid=1;
-j_mid=1;
-j_low=1;
-j_high=length(BRAIN_FILES);    
-while(i_low<=i_high)
-    i_mid = idivide(int32(i_high+i_low),2,'floor');
-    if(alreadyDone(OUT_DIR,OUTER_FILES{i_mid},BRAIN_FILES{j_low})&& ...
-            ~alreadyDone(OUT_DIR,OUTER_FILES{i_mid},BRAIN_FILES{j_high}))
-        % Somewhere on this external node
-        counter = counter+i_mid;
-        disp([num2str(double(counter)/total*100) '%']);
-    else
-        if(alreadyDone(OUT_DIR,OUTER_FILES{i_mid},BRAIN_FILES{j_low}))
-           % After this external node
-           i_low=i_mid+1;
-        else
-           % Before this external node
-           i_high=i_mid-1;
-        end
+% Node Numbers
+nodeFile = 'nodes.mat';
+if(~exist(nodeFile,'file'))
+    BRAIN_DIR='brain';
+    LOAD_DIR='load';
+    OUTER_DIR='outer';
+    INNER_DIR='inner';
+    src_dirs={BRAIN_DIR, LOAD_DIR, OUTER_DIR,INNER_DIR};
+    if(~checkForDirs(src_dirs))
+        return;
     end
-    
-end
-while(j_low<=j_high)
-    j_mid =  idivide(int32(j_high+j_low),2,'floor');
-    if(alreadyDone(OUT_DIR,OUTER_FILES{i_mid},BRAIN_FILES{j_mid}))
-        if(j_mid==length(BRAIN_FILES) || ~alreadyDone(OUT_DIR,OUTER_FILES{i_mid},BRAIN_FILES{j_mid+1}))
-            break;
-        else
-            j_low=j_mid+1;
-        end
-    else
-        j_high=j_mid-1;
-    end
-end
-if(~alreadyDone(OUT_DIR,OUTER_FILES{i_mid},BRAIN_FILES{j_mid}))
-    for i=i_mid:length(OUTER_FILES)
-        for j=j_mid:length(BRAIN_FILES)
-            infl=fullfile(pwd(),OUTER_DIR,OUTER_FILES{i});
-            brainf=fullfile(pwd(),BRAIN_DIR,BRAIN_FILES{j});
-            [outputf,n1,n2]=getOutName(OUTER_FILES{i},BRAIN_FILES{j});
-            outputfullf=fullfile(pwd(),OUT_DIR,outputf);
-            correlate(infl,brainf, outputfullf, n1, n2);
-        end
-        counter = counter+1;
-        disp([num2str(double(counter)/total*100) '%']);
-    end
+    BRAIN_FILES=getNodeFiles(BRAIN_DIR);
+    LOAD_FILES=getNodeFiles(LOAD_DIR);
+    INNER_FILES=getNodeFiles(INNER_DIR);
+    OUTER_FILES=getNodeFiles(OUTER_DIR);
+    disp(['found ' num2str(length(BRAIN_FILES)) ' brain nodes']);
+    disp(['found ' num2str(length(LOAD_FILES)) ' load nodes']);
+    disp(['found ' num2str(length(INNER_FILES)) ' inner nodes']);
+    disp(['found ' num2str(length(OUTER_FILES)) ' outer nodes']);
+    BRAIN_NODES = getNodeNumbers(BRAIN_FILES);
+    LOAD_NODES = getNodeNumbers(LOAD_FILES);
+    INNER_NODES = getNodeNumbers(INNER_FILES);
+    OUTER_NODES = getNodeNumbers(OUTER_FILES);
+    save(nodeFile,'BRAIN_NODES','LOAD_NODES','INNER_NODES','OUTER_NODES');
+    disp(['saved node numbers in ' nodeFile]);
 else
-    counter = counter + i_mid;
-    disp([num2str(double(counter)/total*100) '%']);
-end
-end
-
-function e=checkForDir(d)
-e = exist(d,'dir');
-if(~e)
-    disp(['directory ' d ' does not exist']);
-end
+    disp(['found ' nodeFile]);
+    load(nodeFile); 
 end
 
-function e = checkForDirs(d)
-e = 1;
-for i=1:size(d,2)
-    if(~checkForDir(d{i}))
-        e = 0;
+% Pressure Time Histories
+timeHistFile = 'timehist.mat';
+if(~exist(timeHistFile))
+    PRESSURE = containers.Map;
+    PRESSURE = addNodesToMap(PRESSURE,BRAIN_FILES,BRAIN_DIR);
+    disp('added brain nodes to map');
+    PRESSURE = addNodesToMap(PRESSURE,LOAD_FILES,LOAD_DIR);
+    disp('added load nodes to map');
+    PRESSURE = addNodesToMap(PRESSURE,INNER_FILES,INNER_DIR);
+    disp('added inner nodes to map');
+    PRESSURE = addNodesToMap(PRESSURE,OUTER_FILES,OUTER_DIR);
+    disp('added outer nodes to map');
+    save(timeHistFile,'PRESSURE');
+    disp(['saved node time histories in ' timeHistFile]);
+    rmdir(BRAIN_DIR,'s');
+    rmdir(LOAD_DIR,'s');
+    rmdir(INNER_DIR,'s');
+    rmdir(OUTER_DIR,'s');
+    disp('removed node files');
+else
+    disp(['found ' timeHistFile]);
+    load(timeHistFile);
+end
+
+% Cross Correlate
+xcorrFile = 'xcorr.mat';
+if(~exist(xcorrFile))
+    total = length(BRAIN_NODES)*(length(INNER_NODES)+length(OUTER_NODES));
+    CORRELATIONS = zeros(1,total);
+    count = 0;
+    for i = 1:length(BRAIN_NODES)
+       for j = 1:length(INNER_NODES)
+           count = count + 1;
+           n1 = INNER_NODES(j);
+           n2 = BRAIN_NODES(i);
+           p1 = PRESSURE(num2str(n1));
+           p2 = PRESSURE(num2str(n2));
+           cc = max(xcorr(p1,p2,'coeff'));
+           CORRELATIONS(i*j)=cc;
+           if(mod(j,1000)==0)
+                disp([num2str(count*1.0/total) '%   ' num2str(total-count) ' to go']);
+           end
+       end
+       for j = 1: length(OUTER_NODES)
+           count = count + 1;
+           n1 = OUTER_NODES(j);
+           n2 = BRAIN_NODES(i);
+           p1 = PRESSURE(num2str(n1));
+           p2 = PRESSURE(num2str(n2));
+           cc = max(xcorr(p1,p2,'coeff'));
+           CORRELATIONS(i*j)=cc;
+           if(mod(j,1000)==0)
+                disp([num2str(count*1.0/total) '%   ' num2str(total-count) ' to go']);
+           end
+       end
     end
-end
-end
-
-function nodes = getNodeFiles(d)
-dir_res = dir([d '\NODE*']);
-num_nodes = size(dir_res,1);
-nodes = cell(1,num_nodes);
-for n = 1:num_nodes
-    nodes{n}=dir_res(n).name;
-end
+    save(xcorrFile,'CORRELATIONS');
+else
+    disp(['found ' xcorrFile]);
 end
 
-function [outname, node1,node2] = getOutName(extFile, brnFile)
-n1=extFile(5:length(extFile));
-n2=brnFile(5:length(brnFile));
-outname=[n1 '_' n2];
-node1=n1;
-node2=n2;
 end
 
-function r = alreadyDone(d, externn,brainn)
-n1=externn(5:length(externn));
-n2=brainn(5:length(brainn));
-f = fullfile(pwd(),d,[n1 '_' n2]);
-r=exist(f,'file');
+
+function nodeNums = getNodeNumbers(files)
+  nodeNums = zeros(1,length(files));
+  for i=1:length(files)
+      fname = files{i};
+      nodeNums(i) = str2double(fname(5:length(fname)));
+  end
+end
+
+function map = addNodesToMap(map,FILES,dir)
+  for i=1:length(FILES)
+     FILE = FILES{i};
+     f = fullfile(dir,FILE);
+     data = dlmread(f, '\t', 1, 0);
+     map(FILE(5:length(FILE)))= data(:,3)';
+  end
 end
